3bbc869d7b6ab70293deb40561937a9b44abab7b
rishijohri <coolrishi.johri@gmail.com>
2023-01-07 16:51:14 +0530

    String to string

--- front-end/src/static_module/main/features/example/exampleSlice.ts
+++ front-end/src/static_module/main/features/example/exampleSlice.ts
@@ -1,12 +1,12 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-const initialState: String = "initialState";
+const initialState = "initialState";
 
 export const exampleSlice = createSlice({
     name: "todo",
     initialState,
     reducers: {
-        setExampleAction: (state, action: PayloadAction<String>) => {
+        setExampleAction: (state, action: PayloadAction<string>) => {
             return action.payload;
         }
     }
